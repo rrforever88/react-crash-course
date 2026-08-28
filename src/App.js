36682150c705db48a4b48844cac4549a0c22acd5
Chris Nelson <chris.nelson@sap.com>
2026-08-28 10:35:16 -0400

    Stopped at 56m. Pickup later.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import { useState } from "react"
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([{
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'Feb 5th at 2:30pm',
+    reminder: true,
+},
+{
+    id: 2,
+    text: 'Pick kids up',
+    day: 'Feb 5th at 2:30pm',
+    reminder: true,
+},
+{
+    id: 3,
+    text: 'Get gas',
+    day: 'Feb 5th at 2:30pm',
+    reminder: false,
+}
+])
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
 
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,24 @@
-const Header = () => {
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+//rafce
+
+const Header = ({ title = 'Task Tracker' }) => {
+    const onClick = () => {
+        console.log('click')
+    }
+
   return (
-    <header>
-        <h1>Task Tracker</h1>
+    <header className='header'>
+        <h1>{title}</h1>
+        <Button color='green' text='Add' onClick={onClick} />
     </header>
   )
 }
+
+// CSS in JS
+// const headingStyle = {
+//     color: 'red',
+// }
 
 export default Header
